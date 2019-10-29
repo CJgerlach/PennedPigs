69e4cf31d1e2cf4043c7b0e5066320fadd6511a2
CJgerlach <gerlach@ualberta.ca>
2019-10-29 00:45:02 -0500

    Creation of Activities

---
+++ app/src/main/java/com/example/pennedpigs/SetUpGame.java
@@ -0,0 +1,14 @@
+package com.example.pennedpigs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetUpGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_up_game);
+    }
+}
